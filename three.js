415aba24f4fcc2834fc7a7fdcf4be8c7b29eec76
divine <divineobi07@gmail.com>
2022-05-13 21:58:10 +0100

    fix error message

--- three.js
+++ three.js
@@ -1,13 +1,9 @@
 function success(pos) {
-  console.log(!!pos.coords);
-  if(pos.coords) {
-    document.querySelector('.loading_gif').style.display = 'none';
-    document.getElementById('map').style.display = 'block';
+  if (pos.coords) {
+    document.querySelector(".loading_gif").style.display = "none";
+    document.getElementById("map").style.display = "block";
   }
   let { latitude, longitude } = pos.coords;
-
-
-  console.log(latitude, longitude);
 
   const map = L.map("map").setView([latitude, longitude], 13);
 
@@ -24,6 +20,8 @@ function success(pos) {
 
 function error(err) {
   console.log(err.message);
+  document.getElementById("map").style.display = "none";
+  document.querySelector(".pic").textContent = err.message;
 }
 
 function data() {
